6ba64f0cbda1742a18c1108ed3fc8413ff87ad84
BaoHuy <huyrualuong@gmail.com>
2025-10-25 09:18:51 +0700

    mở cổng 3k

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,9 @@ app.get('/health', (req, res) => {
 const userRoutes = require('./routes/user');
 app.use('/', userRoutes);
 
+
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
+const cors = require('cors');
+app.use(cors({origin: 'http://localhost:3000'}));
