6c602a88c50caae74c69eeaa0d5f30858ae5970e
BaoHuy <huyrualuong@gmail.com>
2025-10-25 08:45:23 +0700

    Mới chỉnh lại cổng 3001

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,6 @@ app.get('/health', (req, res) => {
 const userRoutes = require('./routes/user');
 app.use('/', userRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
